33f0c7dd754966f1937fbd1eb9139b8ebdfeffee
Fulvia <fulvia_@hotmail.it>
2017-06-01 16:28:43 +0200

    Completed desktop and mobile header styles

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -20,6 +20,10 @@ gulp.task('watch', function() {
 	watch('./app/assets/styles/**/*.css', function() {
 		gulp.start('cssInject');
 	});
+
+	watch('./app/assets/scripts/**/*.js', function() {
+		gulp.start('scriptsRefresh');
+	});
 });
 
 // Con il secondo parametro indico a Gulp che prima di
@@ -29,3 +33,9 @@ gulp.task('cssInject', ['styles'], function(){
 	return gulp.src('./app/temp/styles/styles.css')
 		.pipe(browserSync.stream());
 });
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+	browserSync.reload();
+});
+
+
